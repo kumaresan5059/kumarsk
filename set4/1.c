d30a986272a08b49fb60a820f29b61ad65661fff
kumaresan5059 <35220730+kumaresan5059@users.noreply.github.com>
2018-01-19 16:42:59 +0530

    Create 1.c

---
+++ set4/1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	char a[1000];
+	int b=0,c=0,i;
+	gets(a);
+	for(i=0;a[i]!=NULL;i++)
+	{
+		if((a[i]>='a'&&a[i]<='z')||(a[i]>='A'&&a[i]<='Z'))
+		b=++c;
+	}
+	printf("%d",b);
+	return 0;
+}
