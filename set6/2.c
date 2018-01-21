21ec3300d59725eca824278252d8e4f2996c8724
kumaresan5059 <35220730+kumaresan5059@users.noreply.github.com>
2018-01-21 11:57:53 +0530

    Create 2.c

---
+++ set6/2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+ 
+int main() {
+	int n;
+	scanf("%d",&n);
+	switch(n){
+		case 1:
+		printf("one");
+		break;
+		case 2:
+		printf("Two");	break;
+		case 3:
+		printf("Three");	break;
+		case 4:
+		printf("Four");	break;
+		case 5:
+		printf("Five");	break;
+		case 6:
+		printf("Six");	break;
+		case 7:
+		printf("Seven");	break;
+		case 8:
+		printf("Eight");	break;
+		case 9:
+		printf("Nine");	break;
+		case 10:
+		printf("Ten");	break;
+ 
+	}
+	return 0;
+}
+ 
