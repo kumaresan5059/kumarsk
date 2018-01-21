323162726ddbf187a5952dc8fe2233349533f45e
kumaresan5059 <35220730+kumaresan5059@users.noreply.github.com>
2018-01-21 11:09:20 +0530

    Create 8.c

---
+++ Set5/8.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+ 
+int main() {
+	int n,a[n],i,j,t;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(a[i]>a[j])
+			{
+				t=a[i];
+				a[i]=a[j];
+				a[j]=t;
+			}
+		}
+	}
+	printf("%d",a[n/2]);
+	return 0;
+}
